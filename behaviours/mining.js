fc7cafdcda064f8682cb38e438a91c8a1aa5ade0
Nixes <callum@see-bry.com>
2016-10-22 23:34:03 +1100

    start prototyping mining system

--- behaviours/mining.js
+++ behaviours/mining.js
@@ -2,6 +2,23 @@ function mining () {
 
 }
 
+let branch = 0;
+let current_level = 0;
+let number_branches = 0;
+
+mining.BuildMineshaft = function (level) {
+
+}
+
+mining.MineBranch = function () {
+
+}
+
+mining.StartStripMine = function (level, number_branches) {
+  mining.BuildMineshaft(level);
+
+}
+
 mining.BuildStairs = function () {
 
 }
