c4bdbef07c5dad90989635170aec70991661e8e4
Nixes <callum@see-bry.com>
2016-10-23 23:43:47 +1100

    enemy attacking restucture

--- behaviours/survival.js
+++ behaviours/survival.js
@@ -17,7 +17,7 @@ let dirt_like = [
 let distance_close = 10; // these are enemies which can be navigated around
 let distance_danger = 5; // the range at which the bot should attack mobs
 
-
+let currently_attacking_id; // contains the id of the entity that is being attacked, null if not attacking
 
 function survival() {
 
@@ -27,6 +27,14 @@ function DiggingStopped(error) {
   bot.smartChat("Digging interupted: "+error);
 }
 
+bot.on('entityGone', function onGone(entity) {
+    if (currently_attacking_id == null) return;
+    if (entity != currently_attacking_id) return;
+
+    currently_attacking_id = null;
+    survival.SearchEnemies();
+});
+
 survival.IsNight = function() {
   if (bot.time.day > 12000) {
     return true;
@@ -34,6 +42,18 @@ survival.IsNight = function() {
     return false;
   }
 };
+
+// attacks the enemy specified by currently_attacking_id
+survival.AttackEnemy = function (enemy) {
+    bot.moveToTarget(enemy); // move towards the enemy before attacking
+    bot.attack(enemy);
+}
+
+// this when given a list of enemys, will choose the best one to focus on.
+survival.ChooseTarget = function (enemies) {
+  for(enemy of enemies) {
+  }
+}
 
 survival.SearchEnemies = function () {
   let close_enemies = Object.keys(bot.entities).map(function (id) {
@@ -50,6 +70,10 @@ survival.SearchEnemies = function () {
   bot.smartChat('found ' + danger_enemies.length + ' dangerously close enemies');
   //bot.smartChat(close_enemies_ids.join(', '));
   console.log(close_enemies);
+    if (danger_enemies.length > 0) {
+      bot.smartChat("Dangerously close enemies found, attacking.");
+      survival.ChooseTarget(danger_enemies);
+    }
 };
 
 survival.CheckDanger = function() {
@@ -58,27 +82,20 @@ survival.CheckDanger = function() {
   }
 }
 
-survival.ShowHealth = function() {
-  bot.smartChat("Health: "+bot.health);
-  bot.smartChat("Hunger: "+bot.food);
-};
-
-
-
 survival.DigHole = function() {
   // find something dirt like
   let dirt_location = bot.findBlock(
     {
     point: bot.entity.position,
     matching: 3,
-    minDistance: 2,
-    maxDistance: 10
+    maxDistance: 5
   });
 
   console.log(dirt_location);
 
   if (dirt_location !== null) {
-    bot.dig(dirt_location, DiggingStopped );
+    bot.moveToTarget(dirt_location);
+
   }
 
 };
