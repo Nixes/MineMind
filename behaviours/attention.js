9b359c8b267ef9904d97f38a6b64b64a32632db3
Nixes <callum@see-bry.com>
2016-11-08 16:03:23 +1100

    more playing around with attention system structure

--- behaviours/attention.js
+++ behaviours/attention.js
@@ -7,31 +7,50 @@ Bot attention system ideas:
 
 */
 
+class attention_submodule {
+  constructor(name,module_ref,attention) {
+    this.name = name;
+    this.module = module_ref;
+    this.attention = attention;
+  }
+  disable() {
+    this.attention = 0;
+  }
+  increase() {
+
+  }
+  decrease() {
+
+  }
+}
+
 function attention () {
 
 }
 
 /*
-  survival,
-  gathering,
-  crafting
+  attention 0 is disabled
+  attention 1 is most important
+  attention >1 is less important
 */
-attention.goals = [];
 
-attention.Push = function () {};
+attention.submodules = {
+  survival:{module:survival,attention:0},
+  gathering:{module:gathering,attention:0},
+  mining:{module:mining,attention:0},
+};
 
-attention.Pop = function () {};
 
-attention.MoveToTop = function () {
-
+attention.AddModule = function (name,module,starting_attention) {
+  attention.submodules.push( new attention_submodule(name,module,starting_attention) );
 };
 
 // this decides what module should recieve an update next
 attention.Update = function () {
-
 };
 
 // this is called externally by all modules that interact with this system, when they finish their current task
-attention.Return = function () {
-
+attention.Return = function (calling_module_name) {
+  // reset internal timmer
+  attention.Update();
 };
