3b466026c110534d1f7081dc51f4a10078562aa2
Nixes <callum@see-bry.com>
2017-02-01 20:42:20 +1100

    attention now handles entity perception

--- behaviours/attention.js
+++ behaviours/attention.js
@@ -7,6 +7,10 @@ Bot attention system ideas:
 
 */
 
+// hardcoded values for danger radius
+let distance_close = 16; // the range at which awareness of enemies should improve
+let distance_danger = 5; // the range at which the bot should attack mobs
+
 
 function attention () {
   console.log("Attention initialised");
@@ -58,35 +62,90 @@ attention.moveToTarget = function (targetEntity,callback) {
     }
 };
 
+// this function check the updated entity to see if it is of interest to the bot
+attention.ObserveEntity = function (updated_entity) {
+  // check to see if the bot was close enough to be interesting
+  if (bot.entity.position.distanceTo(updated_entity.position) < distance_close) {
+
+    // check to see if it was hostile
+    if (updated_entity.kind === 'Hostile mobs') {
+
+      // then check to see if its going to be a threat
+      if (bot.entity.position.distanceTo(e.position) < distance_danger || e.name === 'Skeleton' || e.name === 'Witch') {
+        // search to see if it needs adding to dangerous targets
+      }
+    }
+
+  } else {
+    // search to see if it needs removal
+  }
+};
+
+// search for nearby entities, this runs on every update, TODO: optimise to allow
+attention.CheckEntities = function () {
+  console.time("entities_search");
+
+  let entity_update_interval = 800;
+
+  let close_entities = Object.keys(bot.entities).map(function (id) {
+    return bot.entities[id];
+  }).filter(function (e) {
+    return bot.entity.position.distanceTo(e.position) < distance_close;
+  });
+  if (close_entities.length > 0) {entity_update_interval = 500;}
+
+  //console.log("Searching for enemies");
+  let close_enemies = close_entities.filter(function (e) {
+    return e.kind === 'Hostile mobs';
+  });
+
+  //console.log('  found ' + close_enemies.length + ' close enemies');
+  if (close_enemies.length > 0) {entity_update_interval = 200;}
+
+  let danger_enemies = close_enemies.filter(function (e) {
+      // add to danger list if normal mob and very close or close and skeleton
+      return bot.entity.position.distanceTo(e.position) < distance_danger || e.name === 'Skeleton' || e.name === 'Witch';
+  });
+  //console.log('  found ' + danger_enemies.length + ' dangerously close enemies');
+  if (danger_enemies.length > 0) {entity_update_interval = 100;}
+
+  // update the list of dangerous entities to allow the survival module to make a decision
+  attention.behaviours.survival.danger_enemies = danger_enemies;
+  //setTimeout(attention.CheckEntities, entity_update_interval);
+
+  console.timeEnd("entities_search");
+};
+
 attention.AddBehaviour = function (name,behaviour) {
   console.log("Adding behaviour "+ name + " to attention.");
   behaviour.return_function = attention.Return;
-  attention.behaviours.push( {name,behaviour} );
+  behaviour.last_run = new Date();
+  attention.behaviours[name] = behaviour;
 };
 
 // this decides what module should recieve an update next
 attention.Update = function () {
   let highest_priority_value = 0;
-  let highest_priority_task;
-  console.log("Behaviours: ");
-  console.log(attention.behaviours);
+  let highest_priority_task_name = "";
+  //console.log("Behaviours: "); console.log(attention.behaviours);
 
   // find the task with the highest priority
-  for (let behaviour of attention.behaviours) {
-    if (behaviour.priority > highest_priority_value) {
-      highest_priority_value = behaviour.priority;
-      highest_priority_task = behaviour;
+  for (let behaviour in attention.behaviours) {
+    if (attention.behaviours[behaviour].priority > highest_priority_value) {
+      highest_priority_value = attention.behaviours[behaviour].priority;
+      highest_priority_task_name = behaviour;
     }
   }
-  console.log("Highest priority task was: ");
-  console.log(highest_priority_task);
+  //console.log("Highest priority task was: " + highest_priority_task_name + " with: " + highest_priority_value);
+
   // run the task
-  highest_priority_task.behaviour.Update();
+  attention.behaviours[highest_priority_task_name].Update();
+  setTimeout(attention.Return, 100);
 };
 
 // this is called externally by all modules that interact with this system, when they finish their current task
 attention.Return = function (calling_module_name) {
-  // reset internal timmer
+  // reset internal timer
   attention.Update();
 };
 
