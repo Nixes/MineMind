13257e29fa96352e0bf495eb0cdb3da23059de8a
Nixes <callum@see-bry.com>
2017-02-01 13:44:34 +1100

    add better checks on behaviour priority changes

--- behaviours/behaviour.js
+++ behaviours/behaviour.js
@@ -1,7 +1,10 @@
 function behaviour () {
 }
+
+// NOTE: All Behaviours must supply their own Update() method and must return to return_function() when they are finished
+
 // public variables, these should be moved into the constructor
-behaviour.priority;
+behaviour.priority = 1;
 behaviour.return_function;
 
 behaviour.setReturnFunction = function (return_function) {
@@ -9,7 +12,18 @@ behaviour.setReturnFunction = function (return_function) {
 };
 
 behaviour.setPriority = function (priority) {
-  this.priority = priority;
+  if (priority > 0) {
+    this.priority = priority;
+  } else {
+    console.log("Something tried to set priority to a negative number");
+  }
+};
+behaviour.setPriorityIfGreater = function (priority) {
+  if (priority > this.priority) {
+    this.priority = priority;
+  } else {
+    console.log("Priority was lesser ignoring");
+  }
 };
 
 behaviour.disable = function () {
@@ -19,7 +33,10 @@ behaviour.increase = function() {
   this.priority++;
 };
 behaviour.decrease = function() {
-  this.priority--;
+  // a priority should never be a negative number
+  if (this.priority !== 0) {
+    this.priority--;
+  }
 };
 
 module.exports = behaviour;
