8f9460f55f61a93a76c230551d15f6d315529e0c
Nixes <callum@see-bry.com>
2017-02-01 13:44:00 +1100

    clean up attention system + add movement responsibilities

--- behaviours/attention.js
+++ behaviours/attention.js
@@ -27,21 +27,61 @@ attention.conflict_priorities = [
   "mining"
 ];
 
-attention.submodules = [];
+attention.behaviours = [];
 
 // this disables the automatic attention process
 attention.command_mode = false;
 
+// only one function can be allowed to perform movements at a time.
+attention.movement_reserved = false;
 
-attention.AddModule = function (module) {
-  attention.submodules.push( new attention_submodule(name,module,starting_attention) );
+attention.moveToTarget = function (targetEntity,callback) {
+    if (targetEntity === null) return;
+
+    var path = bot.navigate.findPathSync(targetEntity.position, {
+        timeout: 1000,
+        endRadius: 2,
+    });
+    if (callback === undefined) {
+        callback = function() { // provide a defualt callback
+            if (targetEntity !== null) {
+                console.log("Finished moving");
+            }
+            // remove reservation
+            attention.movement_reserved = false;
+        };
+    }
+    attention.movement_reserved = true;
+    bot.navigate.walk(path.path, callback);
+    if (path.status !== 'success') {
+      console.log("Pathing failed because: "+ path.status);
+    }
+};
+
+attention.AddBehaviour = function (name,behaviour) {
+  console.log("Adding behaviour "+ name + " to attention.");
+  behaviour.return_function = attention.Return;
+  attention.behaviours.push( {name,behaviour} );
 };
 
 // this decides what module should recieve an update next
 attention.Update = function () {
-  for (let module of submodules) {
-    module.priority
+  let highest_priority_value = 0;
+  let highest_priority_task;
+  console.log("Behaviours: ");
+  console.log(attention.behaviours);
+
+  // find the task with the highest priority
+  for (let behaviour of attention.behaviours) {
+    if (behaviour.priority > highest_priority_value) {
+      highest_priority_value = behaviour.priority;
+      highest_priority_task = behaviour;
+    }
   }
+  console.log("Highest priority task was: ");
+  console.log(highest_priority_task);
+  // run the task
+  highest_priority_task.behaviour.Update();
 };
 
 // this is called externally by all modules that interact with this system, when they finish their current task
@@ -49,3 +89,6 @@ attention.Return = function (calling_module_name) {
   // reset internal timmer
   attention.Update();
 };
+
+
+module.exports = attention;
